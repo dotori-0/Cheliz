5b6757d1e69f767b886f4e6098f797eecfd53d0b
dotori-0 <sesan131@gmail.com>
2022-09-12 17:31:20 +0900

    [Test] API Test

---
+++ Cheliz/Networking/Endpoint.swift
@@ -0,0 +1,13 @@
+//
+//  Endpoint.swift
+//  Cheliz
+//
+//  Created by SC on 2022/09/11.
+//
+
+import Foundation
+
+struct Endpoint {
+    static let multiSearchURL = "https://api.themoviedb.org/3/search/multi?api_key="
+    // https://api.themoviedb.org/3/search/multi?api_key={API_KEY}&language=en-US&query=lucy&page=1&include_adult=false
+}
---
+++ Cheliz/Networking/TMDBAPIManager.swift
@@ -0,0 +1,32 @@
+//
+//  TMDBAPIManager.swift
+//  Cheliz
+//
+//  Created by SC on 2022/09/11.
+//
+
+import Foundation
+
+import Alamofire
+import SwiftyJSON
+
+class TMDBAPIManager {
+    static let shared = TMDBAPIManager()
+    
+    private init() { }
+    
+    func fetchMultiSearchResults(query: String) {
+        let url = Endpoint.multiSearchURL + APIKey.TMDB + "&language=en-US" + "&query=\(query)" + "&page=1"
+        // https://api.themoviedb.org/3/search/multi?api_key={API_KEY}&language=en-US&query=lucy&page=1&include_adult=false
+        
+        AF.request(url, method: .get).validate(statusCode: 200...400).responseData { response in
+            switch response.result {
+                case .success(let value):
+                    let json = JSON(value)
+                    dump(json)
+                case .failure(let error):
+                    print(error)
+            }
+        }
+    }
+}
