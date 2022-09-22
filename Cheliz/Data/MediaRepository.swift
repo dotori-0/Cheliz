7ea78ad71efa4d6e40fa1228a9dc3a3c91675d64
dotori-0 <sesan131@gmail.com>
2022-09-22 19:27:50 +0900

    [Feat] Allow Searching in Korean

--- Cheliz/Data/MediaRepository.swift
+++ Cheliz/Data/MediaRepository.swift
@@ -13,6 +13,8 @@ fileprivate protocol RealmProtocol {
     func add(media: Media, completionHandler: @escaping () -> Void, errorHandler: @escaping () -> Void)
     func delete(media: Media, completionHandler: @escaping () -> Void, errorHandler: @escaping () -> Void)
 //    func sort(by sortingOrder: SortingOrder) -> Results<Media>
+    func sortAndFilter() -> Results<Media>?
+    func toggleWatched(of media: Media, errorHandler: @escaping () -> Void)
 }
 
 struct MediaRepository: RealmProtocol {
